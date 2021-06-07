1fbbbb7ea3a892aba23983998381caddb2eef41a
derogate <afiqf.as@gmail.com>
2021-06-07 19:25:27 +0800

    changed validation message & minor chg email regex

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,9 +8,9 @@ exports.register = async (req, res) => {
 	const { name, email, password } = req.body;
 
 	if (name.trim().length === 0) throw "Please enter your name.";
-	if (name.trim().length < 6) throw "Name must be at least 5 characters or more.";
+	if (name.trim().length < 6) throw "Name must be at least 6 characters or more.";
 	if (email.trim().length === 0) throw "Please enter your email.";
-	const emailRegex = /@gmail.com|@yahoo.com|@hotmail.com|@live.com/;
+	const emailRegex = /@gmail.com|@outlook.com|@hotmail.com|@live.com|@yahoo.com/;
 	if (!emailRegex.test(email)) throw "Email provided is invalid!";
 	if (password.trim().length === 0) throw "Please enter your password.";
 	if (password.trim().length < 8) throw "Password must be at least 8 characters long.";
@@ -42,10 +42,10 @@ exports.login = async (req, res) => {
 	const { email, password } = req.body;
 
 	if (email.trim().length === 0) throw "Please enter your email.";
-	const emailRegex = /@gmail.com|@yahoo.com|@hotmail.com|@live.com/;
+	const emailRegex = /@gmail.com|@outlook.com|@hotmail.com|@live.com|@yahoo.com/;
 	if (!emailRegex.test(email)) throw "Email provided is invalid!";
 	if (password.trim().length === 0) throw "Please enter your password.";
-	if (password.trim().length < 8) throw "Password must be at least 8 characters long.";
+	if (password.trim().length < 8) throw "Password is invalid!";
 
 	const user = await User.findOne({
 		email,
@@ -54,7 +54,7 @@ exports.login = async (req, res) => {
 	const match = await bcrypt.compare(req.body.password, user.password);
 
 	if (!user) throw "Ensure the email is typed correctly.";
-	if (!match) throw "Email and Password do not match!";
+	if (!match) throw "Invalid credentials! Please ensure correct credentials are entered.";
 
 	if (match && user) {
 		const token = await jwt.sign({ id: user.id }, process.env.PRIVATEKEY);
